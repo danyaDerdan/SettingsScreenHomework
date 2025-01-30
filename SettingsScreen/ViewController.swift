32f19affadb82314996aa939489ebf0cdd50d26f
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2025-01-30 18:30:25 +0300

    Deleted storyboard + gitignore

--- SettingsScreen/ViewController.swift
+++ SettingsScreen/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  SettingsScreen
-//
-//  Created by Данил Толстиков on 30.01.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPurple
     }
 
 
